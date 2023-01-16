c335cb12afaa11acc4ca1844cd7cfe38d523d549
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-16 12:20:12 +0100

    JS fonction afficherjourssemaines() pour afficher les jours de semaine

---
+++ job5/script.js
@@ -0,0 +1,9 @@
+
+function afficherjourssemaines(){
+    // var i;
+    jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    for (i = 0; i < 7; i++){
+        console.log(jourssemaines[i]);
+    }
+}
+console.log(afficherjourssemaines());
