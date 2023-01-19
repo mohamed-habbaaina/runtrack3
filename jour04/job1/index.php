56da96fe4d0cd46c2d3065cecc7dbf8be80ff9fc
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-19 17:34:18 +0100

    les promesses Fetch, async & wait

---
+++ jour04/job1/index.php
@@ -0,0 +1,23 @@
+<!-- 
+    Mohamed HABBAAINA Le 19/01/2023
+
+    Créez un <button> ayant comme id “button”. Créez un fichier expression.txt contenant
+votre expression favorite.
+Lorsqu’un utilisateur clique sur le bouton, à l’aide de Fetch, récupérez le contenu du
+fichier expression.txt, placez le dans un paragraphe <p> et insérez le dans le corps de
+votre page.
+ -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job1</title>
+    <button id="button">click</button>
+    <p></p>
+</head>
+<body>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ jour04/job1/script.js
@@ -0,0 +1,17 @@
+async function expression(){
+    const p = document.querySelector('p');
+    
+    await fetch("./expression.txt") //  wait pour que la promesse soit prete
+    .then(async function(response) {    //  async pour utiliser wait il faut que la fonction soit 'asyncron'
+        let html = await response.text();
+        p.innerHTML = html; //  innerHTML interprete les balise html, 
+    })
+
+    .catch(function(err) {
+        console.log('ERREUR : ' + err);
+    });
+    
+}
+let button = document.getElementById("button");
+button.addEventListener('click', expression);
+
