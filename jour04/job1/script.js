44c492c63fa93a5787268a37800bdceb6590071c
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-19 20:08:35 +0100

    ajout commentaire

--- jour04/job1/script.js
+++ jour04/job1/script.js
@@ -2,9 +2,9 @@ async function expression(){
     const p = document.querySelector('p');
     
     await fetch("./expression.txt") //  wait pour que la promesse soit prete
-    .then(async function(response) {    //  async pour utiliser wait il faut que la fonction soit 'asyncron'
+    .then(async function(response) {    //  async pour utiliser await il faut que la fonction soit 'asyncron'
         let html = await response.text();
-        p.innerHTML = html; //  innerHTML interprete les balise html, 
+        p.innerHTML = html; //  innerHTML interprete les balises html, 
     })
 
     .catch(function(err) {
