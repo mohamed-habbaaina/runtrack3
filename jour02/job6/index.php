77c57f75a71773a8588624851ae98171b262fd29
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-19 08:24:35 +0100

    job6 change background-color if code konami matsh

---
+++ jour02/job6/index.php
@@ -0,0 +1,19 @@
+<!-- 
+        Mohamed HABBAAINA Le 17/01/2023
+
+Par défaut, votre index.php n’a pas de contenu.
+Lorsqu’un utilisateur effectue un code konami, la page devient stylisée, aux couleurs de
+La Plateforme_.
+ -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job06</title>
+</head>
+<body>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ jour02/job6/script.js
@@ -0,0 +1,65 @@
+
+const contnt = document.body;  // recupérer la variable dans textarea
+
+var refreshTimer;
+
+let konami = Array();  // tableau pour stoké les input"keyup"
+document.body.addEventListener('keyup', function(event) { // addEventListener quand "keyup"
+  
+  clearTimeout(refreshTimer);
+
+  let result = false;
+  let resultCompeur = 0;  
+  const key = event.key;
+  let compt = konami.length; //  compteur tail de tableau input
+
+  konami.push(key)  // ajouter l'evenement au tableau
+
+
+  if (compt >= 10){  //  if comptur > 10 réinitialiser le tableu et le compteur
+    konami = Array();
+    resultCompeur = 0;
+    contnt.style.backgroundColor = 'white';
+
+  }
+  
+  if(compt == 10){  
+  }
+
+  const codeKonami = [  //  le code konami
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];  //  comparer entre les inputs et le code konani et compté le nombre de matsh
+  for(i=0; i<10; i++){
+    if(konami[i] == codeKonami[i]){
+        resultCompeur++;
+    }
+  if(resultCompeur == 10){
+    
+    result = true;
+  }
+  }
+  if (result == true){
+        // changement de background color si le code Konami est bien rentrer
+        contnt.style.backgroundColor = 'red';
+
+  }
+
+      // timer pour rafriche et réinitialiser le tableu et le compteur si 3 seconde
+  refreshTimer = setTimeout(() => {
+    konami = Array()
+    resultCompeur = 0
+    // contnt.style.backgroundColor = 'white';
+
+  }, 3000);
+  
+
+})
