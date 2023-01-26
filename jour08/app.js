61593c8a8698860a55a5c6251fe0dc8ee362896e
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-26 18:53:42 +0100

    le fichier JS menu hamberger

---
+++ jour08/app.js
@@ -0,0 +1,16 @@
+const toggleMenuBtn = document.querySelector("#menu-btn");
+const toggleMenuImg = document.querySelector("#menu-btn img");
+const toggledMenu = document.querySelector("#toggled-menu");
+const menuLinks = document.querySelector("#main-nav ul a");
+
+toggleMenuBtn.addEventListener("click", function() {
+    toggledMenu.classList.toggle("-translate-y-full");
+    if(toggledMenu.classList.contains("-translate-y-full")){
+        toggleMenuImg.setAttribute("src", "img/fermer.png");
+        toggleMenuBtn.setAttribute("aria-expanded", "false");
+    } else{
+        toggleMenuImg.setAttribute("src", "img/berger.png");
+        toggleMenuBtn.setAttribute("aria-expanded", "true");
+
+    }
+})
