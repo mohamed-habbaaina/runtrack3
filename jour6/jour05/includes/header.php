e5cf3c38092ef6c2ee8d308645bc44c2e550b48a
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-31 17:50:00 +0100

    job5 creation de dossier et le header if isset[login]

---
+++ jour6/jour05/includes/header.php
@@ -0,0 +1,9 @@
+<?php if(!isset($_SESSION['login'])): ?>
+    <nav>
+            <ul>
+                <li>Home</li>
+                <li>Connexion</li>
+                <li>Inscription</li>
+            </ul>
+        </nav>
+<?php endif; ?>
