5f325eb768ec147f6214dbeae818c681141674c1
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-01 21:19:57 +0100

    JS pour l'inscription des utilisateurs

--- jour05/script.js
+++ jour05/script.js
@@ -1,4 +1,22 @@
+// let register = document.querySelector('#btn-register');
+// let formContainer = document.querySelector('#form-register')
 
+// register.addEventListener("click", async function(){
+    
+//     await fetch("inscription.php")
+    
+//     .then((rpns) =>{
+//         if(rpns.ok){
+
+//             return rpns.text();
+//         }
+//     })
+
+//     .then((html) =>{
+//         formContainer.innerHTML = html;
+//         console.log(formContainer);
+//     })
+// })
 const form = document.querySelector('#form-register');
 
     form.addEventListener('submit', async function(e){
